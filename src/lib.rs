7dde77e8dd428ba06eb539e74c8ddc943d18c953
meloalright <meloalright@gmail.com>
2022-08-29 23:32:08 +0800

    + default_random + format

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,33 @@ use rand::{
     Rng, SeedableRng,
 };
 
+pub fn format(count: usize, random: fn(usize) -> Vec<u8>) -> String {
+    assert!(count > 0, "The count should at least greater than 0");
 
-pub fn colorid(size: usize) -> String {
+    let buffer: Vec<u8> = random(count * 3);
+    let mut id: String = String::with_capacity(count * 7 - 1);
+
+    for i in 0..count {
+        let color = hex::encode_upper(&buffer[i*3..i*3+3]);
+        id.push_str(&color);
+        if i + 1 < count {
+            id.push('-');
+        }
+    }
+    id
+}
+
+pub fn default_random(size: usize) -> Vec<u8> {
     let mut rng = StdRng::from_entropy();
-    let mut buffer: Vec<u8> = vec![0; size * 3];
+    let mut buffer: Vec<u8> = vec![0; size];
 
     rng.fill(&mut buffer[..]);
+    buffer
+}
 
-    let result: String = {
-        let mut color_string: String = "".to_owned();
-        for i in 0..size {
-            let color = hex::encode_upper(&buffer[i*3..i*3+3]);
-            color_string.push_str(&color);
-            if i != size - 1 {
-                color_string.push_str("-");
-            }
-        }
-        color_string
-    };
 
-    result
+pub fn colorid(count: usize) -> String {
+    format(count, default_random)
 }
 
 #[cfg(test)]
@@ -31,10 +38,15 @@ mod test_color {
     use super::*;
 
     #[test]
-    fn size_len() {
+    fn test_colorid() {
         assert_eq!(colorid!().len(), 27);
         assert_eq!(colorid(5).len(), 34);
-        assert_eq!(colorid(0).len(), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_colorid() {
+        let _ = colorid(0);
     }
 }
 
